0d2e3e36a5a948dba7c65c62983fff8114d889df
Khairunnisa Isma Hanifah <49370237+KhairunnisaIsma@users.noreply.github.com>
2025-10-11 22:08:05 +0700

    Update models.py

--- files/models.py
+++ files/models.py
@@ -1690,6 +1690,7 @@ class HomepagePopup(models.Model):
 
     popup = models.FileField(
         "popup",
+        upload_to="homepage-popups/",
         help_text="Only this image will appear on the pop-up. Ideal image size is 900 x 650 pixels",
         max_length=500,
     )
