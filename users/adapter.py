169331935767f02749f82ebc47b92cb99df1ebe8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2025-10-11 09:13:31 +0700

    chore: remove debug print and breakpoint

--- users/adapter.py
+++ users/adapter.py
@@ -14,12 +14,12 @@ from .models import BlackListedEmail
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def encrypt(self, text: str):
-        encrypted_bytes = self.cipher_suite.encrypt(text.encode('utf-8'))
-        return encrypted_bytes.decode('utf-8')
-    
+        encrypted_bytes = self.cipher_suite.encrypt(text.encode("utf-8"))
+        return encrypted_bytes.decode("utf-8")
+
     def decrypt(self, encrypted_text: str):
-        decrypted_bytes = self.cipher_suite.decrypt(encrypted_text.encode('utf-8'))
-        return decrypted_bytes.decode('utf-8')
+        decrypted_bytes = self.cipher_suite.decrypt(encrypted_text.encode("utf-8"))
+        return decrypted_bytes.decode("utf-8")
 
     def get_email_confirmation_url_stub(self, request, emailconfirmation):
         url = reverse("account_confirm_email", args=[emailconfirmation.key])
@@ -44,25 +44,22 @@ class MyAccountAdapter(DefaultAccountAdapter):
             "current_site": site,
         }
         ctx.update(context)
-        
+
         msg = self.render_mail(template_prefix, email, ctx)
-        print("Debug: email_message_subject", msg.subject)
-        print("Debug: email_message_body", msg.body)
-        breakpoint()
         msg.send(fail_silently=True)
 
     def get_login_redirect_url(self, request):
         if user_requires_mfa(request.user):
             mfa_enabled = is_mfa_enabled(request.user)
             if not mfa_enabled:
-                return resolve_url('/accounts/2fa/totp/activate')
-            return resolve_url('/')
-        return resolve_url('/')
+                return resolve_url("/accounts/2fa/totp/activate")
+            return resolve_url("/")
+        return resolve_url("/")
 
     @property
     def key(self):
         return generate_key()
-    
+
     @property
     def cipher_suite(self):
         return generate_cipher(self.key)
