7becf83d7368159bf326f5aa656c620d35263ad3
Bea M <64287361+beamariano@users.noreply.github.com>
2025-10-14 13:41:41 +0800

    chore: remove unreachable code

--- users/validators.py
+++ users/validators.py
@@ -4,7 +4,6 @@ from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
 from django.core.exceptions import ValidationError
-from django.conf import settings
 
 
 @deconstructible
