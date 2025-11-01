364dc31e920a73b39598b8e11d15097e7d05e145
Mico Balina <mico.balina@cdts.com.ph>
2025-11-02 01:58:25 +0800

    Adjustted alignment of arrows according to Penpot

--- frontend/src/static/js/pages/components/MediaListHeader.js
+++ frontend/src/static/js/pages/components/MediaListHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 export function MediaListHeader(props){
 	return (<><div className={ ( void 0 === props.className || null === props.className ? '' : props.className + ' ' ) + 'hw-category' } style={ props.style }>
-				<h2 className="hw-category-title">{ props.title }</h2>
+				<h1 className="hw-category-title">{ props.title }</h1>
 				{ void 0 === props.viewAllLink || null === props.viewAllLink ? null : <a href={ props.viewAllLink } title={ props.viewAllText || props.viewAllLink } className="hw-category-link">{ props.viewAllText || props.viewAllLink }</a> }
 			</div>
 			{ props.desc && <div className="hw-category-description" dangerouslySetInnerHTML={{ __html: props.desc }}></div> }
