c5ffa32101b6f0d78b26cf504b18c12db6e88ccc
andriawan24 <fawaznaufal23@gmail.com>
2026-05-09 09:42:46 +0700

    feat: Implement dark mode for ripple assets

--- frontend/src/features/layout/components/RippleDecoration.jsx
+++ frontend/src/features/layout/components/RippleDecoration.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 export function RippleDecoration() {
 	return (
 		<div className="app-layout__ripple">
-			<img className="app-layout__ripple-image" src="./static/images/img_ripple.webp" alt="" aria-hidden="true" />
+			<img
+				className="app-layout__ripple-image app-layout__ripple-image--light-theme"
+				src="./static/images/img_ripple_dark.webp"
+				alt=""
+				aria-hidden="true"
+			/>
+			<img
+				className="app-layout__ripple-image app-layout__ripple-image--dark-theme"
+				src="./static/images/img_ripple.webp"
+				alt=""
+				aria-hidden="true"
+			/>
 		</div>
 	);
 }
