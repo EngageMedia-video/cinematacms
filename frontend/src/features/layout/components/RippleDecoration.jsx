865bf8422715aa6be3ecb49120884c83a0eae20d
andriawan24 <fawaznaufal23@gmail.com>
2026-05-10 10:43:58 +0700

    fix: Clarify naming for img_ripple_light and img_ripple_dark

--- frontend/src/features/layout/components/RippleDecoration.jsx
+++ frontend/src/features/layout/components/RippleDecoration.jsx
@@ -5,13 +5,13 @@ export function RippleDecoration() {
 		<div className="app-layout__ripple">
 			<img
 				className="app-layout__ripple-image app-layout__ripple-image--light-theme"
-				src="./static/images/img_ripple_dark.webp"
+				src="/static/images/img_ripple_light.webp"
 				alt=""
 				aria-hidden="true"
 			/>
 			<img
 				className="app-layout__ripple-image app-layout__ripple-image--dark-theme"
-				src="./static/images/img_ripple.webp"
+				src="/static/images/img_ripple_dark.webp"
 				alt=""
 				aria-hidden="true"
 			/>
