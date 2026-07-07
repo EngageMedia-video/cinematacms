9c52def47228f2b0439a2e8f372f60dbb5fba19c
Adryan Eka Vandra <adryanekavandra@gmail.com>
2026-07-07 23:58:46 +0700

    add django observability instrumentation

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -4,4 +4,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms.settings")
 
+try:
+    import uwsgi
+    from prometheus_client.values import MultiProcessValue
+
+    MultiProcessValue(process_identifier=lambda: uwsgi.worker_id())
+except (ImportError, ModuleNotFoundError):
+    pass
+
 application = get_wsgi_application()
